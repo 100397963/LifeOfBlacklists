1a8813c940e50b3cd5ad705f6ff51d34a0f391c1
100397963 <47381623+100397963@users.noreply.github.com>
2019-02-09 21:17:33 +0100

    Develop commit version #2.2 @ToBeFixed
    Global dump of information
    PARSING ISSUES

--- MainProto.py
+++ MainProto.py
@@ -18,7 +18,7 @@ import datetime
 
 # Globals
 
-currDay = datetime.datetime.now().strftime("%Y-%m-%d")
+currDay = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 path = os.path.realpath(__file__).split("MainProto")[0]
 
 # Url list import
@@ -30,7 +30,7 @@ def importUrls():
     with open(importPath) as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            results.append(str(row)[2:-2])
+            results.append(str(row)[2:-2].split(";"))
     return results
 
 # Url entry parsing
@@ -38,39 +38,62 @@ def importUrls():
 
 def validEntry(string):
 
-    if len(string) == 0: return 1
-    if string[0] == '#': return 1
-    if string[0] == '-': return 1
-    if string[0] == '$': return 1
-    if string[0] == '\"': return 1
-    if string[0] == '/': return 1
-
-    return 0
+    if len(string) == 0: return 0
+    if string[0] == '#': return 0
+    if string[0] == '-': return 0
+    if string[0] == '$': return 0
+    if string[0] == '\"': return 0
+    if string[0] == '/': return 0
+    if string[0] == '<': return 0
+    return 1
 
 # Content extraction from urls
 
-def getContents(url):
-    contents = urllib.request.urlopen(url).read()
-    decoded = contents.decode("utf-8")
-    decodedArray = decoded.split("\n")
+def getContents(url, filePath, category, index):
 
-    # Conversion and saving
+    #404 error protection
+    try:
+        contents = urllib.request.urlopen(url[0]).read()
+        decoded = contents.decode("utf-8")
+        decodedArray = decoded.split("\n")
 
-    dirPath = path + 'Outputs'
-
-    if not os.path.isdir(dirPath):
-        os.mkdir(dirPath)
-
-    testFile = open(dirPath + '\\File1.txt', "a+")
-    for entry in decodedArray:
-        if validEntry(entry):
-            testFile.write(entry + ';' + url + ';' + currDay + '\n')
-    return
-
+        testFile = open(filePath + "\\" + str(index) + ".txt", "w+")
+        for entry in decodedArray:
+            if validEntry(entry):
+                testFile.write(str((entry + ';' + url[0] + ';' + currDay + ';' + category + '\n').encode("utf-8")))
+        return
+    except:
+        print("40x http error")
 # Program Main Flow
 url = importUrls()
 
-for entry in url :
-    getContents(entry)
+# Conversion and saving
+
+dirPath = path + 'Outputs'
+
+if not os.path.isdir(dirPath):
+    os.mkdir(dirPath)
+
+dirPath = dirPath + '\\' + datetime.datetime.now().strftime("%Y-%m-%d")
+
+if not os.path.isdir(dirPath):
+    os.mkdir(dirPath)
+
+category = ""
+filePath = ""
+index = 1
+
+for entry in url:
+
+    if not entry[1] == category:
+        if not os.path.isdir(dirPath + "\\" + entry[1]):
+            os.mkdir(dirPath + "\\" + entry[1])
+        category = entry[1]
+    filePath = dirPath + "\\" + entry[1]
+
+    getContents(entry, filePath, category, index)
+    print(index)
+    index = index + 1
+
 
 
